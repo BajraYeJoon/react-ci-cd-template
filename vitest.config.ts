d98940c9ce593a20f22f4ef040a0ecfbfd8265ee
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-07-18 16:13:33 +0545

    chore: Update vitest.config.ts to use "text" reporter for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       extension: [".tsx"],
-      reporter: ["text-summary", "json-summary", "json"],
+      reporter: ["text", "json-summary", "json"],
     },
   },
 });
