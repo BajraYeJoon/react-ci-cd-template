e14fe63dacb34ad356a59af1da4c911a1810238d
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-07-18 15:26:57 +0545

    chore: test case and husky installed

--- src/__test__/defaultPage.test.tsx
+++ src/__test__/defaultPage.test.tsx
@@ -1,5 +1,5 @@
 // App.test.tsx
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest";
 import App from "../App";
 
